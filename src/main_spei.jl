2c6fead88f8e82aac0f141cf962332bc20c5c842
Dongdong Kong <kongdd@users.noreply.github.com>
2024-04-08 09:36:01 +0800

    separate cal_param

--- src/main_spei.jl
+++ src/main_spei.jl
@@ -1,20 +1,40 @@
+function param_spei(x_ref::AbstractVector)
+  x2 = x_ref[.!isnan.(x_ref)] |> sort # have to sort
+  beta = pwm(x2, 0.0, 0.0, 0)
+  params = _fit_logLogistic(beta)
+  params
+end
+
+
+# add a reference function to spi
+function param_spi(x_ref::AbstractVector; fit="lmom")
+  x2 = x_ref[.!isnan.(x_ref)]
+  x_nozero = x2[x2.>0]
+  q = 1 - length(x_nozero) / length(x2) # probability of zero
+
+  if fit == "mle"
+    D = fit_mle(Gamma, x_nozero)
+  else
+    p = fit_gamma(x_nozero)
+    D = Gamma(p...)
+  end
+  params(D), q
+end
+
+
 """
     spei(x::AbstractVector)
 """
-function spei(x::AbstractVector)
-  x2 = x[.!isnan.(x)] |> sort
-  beta = pwm(x2, 0.0, 0.0, 0)
-  params = _fit_logLogistic(beta)
+function spei(x::AbstractVector, x_ref::AbstractVector=x)
+  params = param_spei(x_ref)
+  z = zeros(Float64, length(x))
 
-  n = length(x)
-  z = zeros(Float64, n)
-  for i in 1:n
+  for i in eachindex(z)
     prob = cdf_logLogistic(x[i], params)
     z[i] = -invcdf_standardGaussian(prob)
   end
   (; z, coef=params)
 end
-
 
 # 这里需要提供一个reference period
 """
@@ -25,18 +45,10 @@ end
   + `lmom`: use lmoments to fit gamma distribution, R version
   + `mle`: maximum likelihood estimation, julia solver
 """
-function spi(x::AbstractVector; fit="lmom")
-  x2 = x[.!isnan.(x)]
-  x_nozero = x2[x2.>0]
-  q = 1 - length(x_nozero) / length(x2) # probability of zero
+function spi(x::AbstractVector, x_ref::AbstractVector=x; fit="lmom")
+  param, q = param_spi(x_ref; fit)
+  D = Gamma(param...)
   
-  if fit == "mle"
-    D = fit_mle(Gamma, x_nozero)
-  else
-    p = fit_gamma(x_nozero)
-    D = Gamma(p...)
-  end
-
   z = fill(NaN, length(x))
   for i in eachindex(z)
     if !isnan(x[i])
@@ -47,3 +59,5 @@ function spi(x::AbstractVector; fit="lmom")
   end
   (; z, coef=params(D))
 end
+
+# TODO: 需要添加scale参数
--- test/test-spei.jl
+++ test/test-spei.jl
@@ -1,12 +1,16 @@
 using Test
 using RCall
+using SPEI
 
+function nanmax(x)
+  x2 = x[.!isnan.(x)]
+  maximum(x2)
+end
 
 R"""
 library(SPEI2)
 """
 wb = R"wb" |> rcopy
-
 
 @testset "spei" begin
   @time r_R = R"cal_spei(wb)" |> rcopy
@@ -18,7 +22,6 @@ wb = R"wb" |> rcopy
   @test maximum(abs.(e)) <= 1e-3
 end
 
-
 @testset "spi" begin
   @time r_R = R"cal_spi(wb)" |> rcopy
   @time r_jl = spi(wb)
@@ -26,15 +29,16 @@ end
   z_r = r_R[:z]
   z_jl = r_jl.z
   e = z_r - z_jl
+  @test abs(nanmax(e)) <= 1e-6
 end
 
+# > 计算结果完全一致
 
+# ## 误差还挺大的
+# begin
+#   using Plots
 
-## 误差还挺大的
-begin
-  using Plots
-
-  plot(z_r, label="R")
-  plot!(z_jl, label="Julia")
-  plot!(e, label="R - Julia")
-end
+#   plot(z_r, label="R")
+#   plot!(z_jl, label="Julia")
+#   plot!(e, label="R - Julia")
+# end
